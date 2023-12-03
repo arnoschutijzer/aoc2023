a102d8d06d88b8cd731f49083efbf9ddd0087d44
Arno Schutijzer <arno.schutijzer@skryv.com>
2023-12-03 16:46:15 +0100

    fix test + typos

--- day2/day2_test.go
+++ day2/day2_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Day1Part1FirstGameIsPossible(t *testing.T) {
+func Test_Day2Part1FirstGameIsPossible(t *testing.T) {
 	games := ReadGames("./fixtures/example.txt")
 	isPotentialGame := IsPossibleGame(games[0], map[string]int{
 		"red":   12,
@@ -16,7 +16,7 @@ func Test_Day1Part1FirstGameIsPossible(t *testing.T) {
 	assert.True(t, isPotentialGame)
 }
 
-func Test_Day1Part1SecondGameIsPossible(t *testing.T) {
+func Test_Day2Part1SecondGameIsPossible(t *testing.T) {
 	games := ReadGames("./fixtures/example.txt")
 	isPotentialGame := IsPossibleGame(games[1], map[string]int{
 		"red":   12,
@@ -26,7 +26,7 @@ func Test_Day1Part1SecondGameIsPossible(t *testing.T) {
 	assert.True(t, isPotentialGame)
 }
 
-func Test_Day1Part1ThirdGameIsNotPossible(t *testing.T) {
+func Test_Day2Part1ThirdGameIsNotPossible(t *testing.T) {
 	games := ReadGames("./fixtures/example.txt")
 	isPotentialGame := IsPossibleGame(games[2], map[string]int{
 		"red":   12,
@@ -36,7 +36,7 @@ func Test_Day1Part1ThirdGameIsNotPossible(t *testing.T) {
 	assert.False(t, isPotentialGame)
 }
 
-func Test_Day1Part1ExampleHas8(t *testing.T) {
+func Test_Day2Part1ExampleHas8(t *testing.T) {
 	games := ReadGames("./fixtures/example.txt")
 	sum := CalculateGameSum(games, map[string]int{
 		"red":   12,
@@ -46,7 +46,7 @@ func Test_Day1Part1ExampleHas8(t *testing.T) {
 	assert.Equal(t, 8, sum)
 }
 
-func Test_Day1Part1(t *testing.T) {
+func Test_Day2Part1(t *testing.T) {
 	games := ReadGames("./fixtures/part1.txt")
 	sum := CalculateGameSum(games, map[string]int{
 		"red":   12,
@@ -56,14 +56,14 @@ func Test_Day1Part1(t *testing.T) {
 	assert.Equal(t, 2317, sum)
 }
 
-func Test_Day1Part1FirstGameHas48Power(t *testing.T) {
+func Test_Day2Part1FirstGameHas48Power(t *testing.T) {
 	games := ReadGames("./fixtures/example.txt")
 	totalPower := GetTotalPowerOfSet(games[0])
 	assert.Equal(t, 48, totalPower)
 }
 
-func Test_Day1Part2(t *testing.T) {
+func Test_Day2Part2(t *testing.T) {
 	games := ReadGames("./fixtures/part1.txt")
 	sum := CalculateGameSumPart2(games)
-	assert.Equal(t, 2317, sum)
+	assert.Equal(t, 74804, sum)
 }
